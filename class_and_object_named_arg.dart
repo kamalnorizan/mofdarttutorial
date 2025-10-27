1e036c6c9b9b876aac902f037cad4fbac1a96ca1
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-10-27 15:08:53 +0800

    nullable with default value

--- class_and_object_named_arg.dart
+++ class_and_object_named_arg.dart
@@ -1,17 +1,25 @@
 void main() {
-  Car mycar1 = new Car(color: 'Red', tyres: 4, price: 180000.00, model:'X70');
+  Car mycar1 = new Car(color: 'Red', tyres: 4, price: 180000.00, model: 'X70');
+  Car mycar2 = new Car(tyres: 4, price: 180000.00, model: 'X70');
 
   mycar1.printCar();
+  mycar2.printCar();
 }
 
 class Car {
   int tyres = 5;
-  String color = 'Black';
+  String? color;
   String? type;
   double price = 100000.00;
   String model = 'Audi';
 
-  Car({required this.tyres, required this.color, this.type, required this.price, required this.model});
+  Car({
+    required this.tyres,
+    this.color = 'Black',
+    this.type,
+    required this.price,
+    required this.model,
+  });
 
   printCar() {
     print('Car type is: $type');
