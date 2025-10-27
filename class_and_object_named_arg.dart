b549ff0d741ba1f38074499553930c18b94ddf67
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-10-27 15:38:46 +0800

    null check and nested class

--- class_and_object_named_arg.dart
+++ class_and_object_named_arg.dart
@@ -2,8 +2,25 @@ void main() {
   Car mycar1 = new Car(color: 'Red', tyres: 4, price: 180000.00, model: 'X70');
   Car mycar2 = new Car(tyres: 4, price: 180000.00, model: 'X70');
 
-  mycar1.printCar();
-  mycar2.printCar();
+  Person person1 = new Person(
+    'John',
+    'Doe',
+    cars: [
+      mycar1,
+      new Car(tyres: 5, price: 190000.00, model: 'X50'),
+    ],
+  );
+
+  person1.printPerson();
+
+  print('--- Individual Cars ---');
+
+  Person person2 = new Person('Jane', 'Smith');
+  person2.printPerson();
+
+  // mycar1.printCar();
+  // mycar2.printCar();
+  print('--- End of Program ---');
 }
 
 class Car {
@@ -26,5 +43,22 @@ class Car {
     print('Car model is: $model');
     print('Car color is: $color');
     print('Car price is: $price');
+  }
+}
+
+class Person {
+  String firstName;
+  String lastName;
+  List<Car>? cars;
+
+  Person(this.firstName, this.lastName, {this.cars});
+
+  printPerson() {
+    print('Person name is: $firstName $lastName');
+    if (cars != null) {
+      for (var car in cars!) {
+        car.printCar();
+      }
+    }
   }
 }
